baee629d1c96f09e5cc855b4b83be0eed4efb854
Álvaro Castro Castilla <a@fourthbit.com>
2020-03-07 22:41:16 +0200

    First cut of UI

--- src/clj/dfym/usecases/html.clj
+++ src/clj/dfym/usecases/html.clj
@@ -9,10 +9,17 @@
     [:head
      [:meta {:charset "utf-8"}]
      [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
-     ;; [:link {:rel "stylesheet" :href "/css/antd.min.css"}]
      ;; Normalize
-     [:link {:rel "stylesheet" :href "css/normalize.css"}]]
+     [:link {:rel "stylesheet" :href "css/normalize.css"}]
+     [:link {:rel "stylesheet" :href "css/simplegrid.css"}]
+     ;; Media player
+     [:script {:src "mediaplayer/mediaelement-and-player.min.js" :type "text/javascript"}]
+     [:link {:rel "stylesheet" :href "mediaplayer/mediaelementplayer.min.css"}]
+     [:link {:rel "stylesheet" :href "css/mediaplayer.css"}]]
     [:body
      [:div#sente-csrf-token {:data-csrf-token csrf-token}]
      [:div#app]
+     ;; Media player
+     [:script {:src "https://unpkg.com/mediaplayer/browser.js" :type "text/javascript"}]
+     ;; App
      [:script {:src "js/dfym.js" :type "text/javascript"}]]]))
--- src/cljs/dfym/core.cljs
+++ src/cljs/dfym/core.cljs
@@ -7,6 +7,7 @@
    [taoensso.sente.packers.transit :as sente-transit]
    [rum.core :as rum]
    [garden.core :refer [css]]
+   [garden.stylesheet :as stylesheet]
    [goog.style]
    [goog.string :as gstring]
    ;; -----
@@ -55,8 +56,9 @@
          (do (when handler (handler resp))
              (when cb (cb resp))))))))
 
-(def get-user
-  (build-request :user/get
+(defn get-user [user]
+  (js/alert "HELLO")
+  #_(build-request :user/get
                  (fn [args] (log* "RECEIVED" args))))
 
 ;;
@@ -64,7 +66,64 @@
 ;;
 
 (def styles
-  (css [:h1 {:font-weight "bold"}]))
+  (css
+   (stylesheet/at-font-face {:font-family "VT323"
+                             :src "url(\"/fonts/VT323-Regular.ttf\")"})
+   [:body :h1 :h2 :h3 :h4 :h5 :h6 {:font-family "VT323"}]
+   [:h1 {:font-size "24px"}]
+   [:h2 {:font-size "16px"
+         :margin "0 0 10px 0"}]
+   [:h3 {:font-size "18px"
+         :padding "5px"
+         :margin "5px"}]
+   [:.fright {:float "right"}]
+   [:.top-operations {:float "right"
+                      :font-size "14px"
+                      :font-width "normal"
+                      :margin "0 10px 0 0"
+                      :cursor "pointer"}]
+   [:.tag {:font-size "18px"
+           :font-weight "normal"
+           :padding "5px"
+           :margin "5px 10px 5px 10px"
+           :border-style "solid"
+           :border-width "2px"
+           :cursor "pointer"}]
+   [:.file {:font-size "18px"
+            :font-weight "normal"
+            :padding "0px"
+            :margin "5px 10px 15px 10px"
+            ;;:border-bottom "dotted"
+            ;;:border-width "2px"
+            :cursor "pointer"}]
+   [:.dir {;:border-bottom "1px solid"
+           :font-weight "normal"}]
+   [:.selected {:border-bottom "2px solid"
+                :font-weight "bold"}]
+   [:.scroll {:overflow "auto"}]
+   [:.no-scroll {:overflow "hidden"}]
+   [:.border {:border-style "solid"
+              :border-width "2px"}]
+   [:.panel {:overflow-y "scroll"
+             :height "100%"}]
+   [:.panel-bottom {:height "70px"}]
+   [:#controls {:width "100%"
+                :position "fixed"
+                :bottom 0}]
+   [:#player {:width "-webkit-calc(100% - 40px); -moz-calc(100% - 40px); calc(100% - 40px)"
+              :float "left"
+              :border-top "solid 2px"
+              :border-bottom "solid 2px"}]
+   [:#menu-button {:float "left"
+                   :width "40px"
+                   :height "40px"
+                   :background-color "white"
+                   :border-top "solid 2px"
+                   :border-bottom "solid 2px"
+                   :font-size "30px"
+                   :text-align "center"
+                   :line-height "40px"
+                   :cursor "pointer"}]))
 
 (defonce style-node (atom nil))
 (if @style-node
@@ -72,14 +131,96 @@
   (reset! style-node (goog.style/installStyles styles)))
 
 (rum/defc app []
-  [:section.section>div.container
-   [:h1.title "HELLO"]
-   [:p.subtitle {:on-click #(get-user 0)} "Let's go!"]])
+  [:div.no-scroll {:style {:width "100%" :height "100%"}}
+   [:div {:style {:padding "20px"}}
+    [:div.col-1-4 {:style {:padding "0 8px 0 20px"}}
+     [:div.panel
+      [:h2 "TAGS"]
+      [:div
+       [:.tag "[Untagged]"]
+       [:.tag "Classical"]
+       [:.tag "Ambient/Experimental"]
+       [:.tag "Work"]
+       [:.tag "Energetic"]
+       [:.tag "Happy"]
+       [:.tag "Death Metal"]
+       [:.tag "Metal"]
+       [:.tag "Black Metal"]
+       [:.tag "Weird"]
+       [:.tag "Mathematical"]
+       [:.tag "Beautiful"]
+       [:.tag "Complex"]
+       [:.tag "Disgusting"]
+       [:.tag "Should remove"]
+       [:.tag "Apocalyptical"]
+       [:.tag "Noise"]
+       [:.tag "Sophisticated"]]
+      [:div.panel-bottom]]]
+    [:div.col-1-4 {:style {:padding "0 8px 0 0px"}}
+     [:div.panel
+      [:h2 "SELECTED TAGS"]
+      [:div [:.tag "Mathematical"]]
+      [:div.panel-bottom]]
+     [:.div.panel-bottom]]
+    [:div.col-2-4 {:style {:width "50%"}}
+     [:div.panel
+      [:div
+       [:h2 "FILTERED FILES"
+        [:div.top-operations "Deselect_All"]
+        [:div.top-operations "Select_All"]]]
+      [:div
+       [:.file.dir "▨ [Yury Stravisnky] Songs of Komogorov"]
+       [:.file.dir "▨ The biggest pain in the Ass [The Beatles]"]
+       [:.file.dir.selected "■ MIDI shits I produced when I was on LSD"]
+       [:.file.dir "▨ Weird sounds and forbidden music"]
+       [:.file.dir "▨ [Yury Stravisnky] Songs of Komogorov"]
+       [:.file.dir "▨ The biggest pain in the Ass [The Beatles]"]
+       [:.file.dir "▨ The biggest pain in the Ass [The Beatles]"]
+       [:.file.dir "▨ [Yury Stravisnky] Songs of Komogorov"]
+       [:.file.dir "▨ The biggest pain in the Ass [The Beatles]"]
+       [:.file.dir "▨ The biggest pain in the Ass [The Beatles]"]
+       [:.file.dir "▨ [Yury Stravisnky] Songs of Komogorov"]
+       [:.file.dir "▨ The biggest pain in the Ass [The Beatles]"]
+       [:.file.dir "▨ The biggest pain in the Ass [The Beatles]"]
+       [:.file.dir "▨ MIDI shits I produced when I was on LSD"]
+       [:.file.dir "▨ Weird sounds and forbidden music"]
+       [:.file.dir "▨ [Yury Stravisnky] Songs of Komogorov"]
+       [:.file.dir "▨ MIDI shits I produced when I was on LSD"]
+       [:.file "□ Weird sounds and forbidden music"]
+       [:.file "□ Weird sounds and forbidden music"]
+       [:.file.selected "■ Weird sounds and forbidden music"]
+       [:.file.selected "■ [Yury Stravisnky] Songs of Komogorov"]
+       [:.file.selected "■ Weird sounds and forbidden music"]
+       [:.file "□ Weird sounds and forbidden music"]
+       [:.file "□ Weird sounds and forbidden music"]
+       [:.file "□ [Yury Stravisnky] Songs of Komogorov"]
+       [:.file "□ Weird sounds and forbidden music"]
+       [:.file "□ Weird sounds and forbidden music"]
+       [:.file "□ [Yury Stravisnky] Songs of Komogorov"]
+       [:.file "□ The biggest pain in the Ass [The Beatles]"]
+       [:.file "□ MIDI shits I produced when I was on LSD"]
+       [:.file "□ Weird sounds and forbidden music"]]
+      [:.div.panel-bottom]]]]
+   [:div#controls
+    [:div#player
+     [:audio {:controls "controls"}
+      [:source {:src "https://www.dropbox.com/s/12fpcuwwmg8s7aj/02%20-%20Theme%20From%20Jack%20Johnson.mp3?raw=1"}]]]
+    [:div#menu-button {:on-click #(get-user 0)} "⚙"]]])
+
+(defn make-player [player-html-element]
+  (let [player (js/MediaElementPlayer.
+                player-html-element
+                (clj->js {:stretching "responsive"
+                          :preload "auto"
+                          :features ["playpause" "current" "progress" "tracks" "duration"]}))]
+    (.addEventListener player-html-element "ended" #(js/alert "NEXT"))))
 
 ;;
 ;; Init
 ;;
 
 (rum/mount (app) (js/document.getElementById "app"))
+
+(make-player (js/document.querySelector "audio[controls], video[controls]"))
 
 (client/start-router!)
